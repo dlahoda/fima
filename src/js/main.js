d8df18f905b9bac1c4db271360b2e8d2b08e178c
Denys Lahoda <denys.lahoda@gmail.com>
2019-01-09 16:23:53 +0200

    Added slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,4 @@
 //= "../../node_modules/jquery/dist/jquery.js"
 //= "../../node_modules/popper.js/dist/umd/popper.js"
+//= "../../node_modules/bootstrap/js/dist/util.js"
+//= "../../node_modules/bootstrap/js/dist/carousel.js"
