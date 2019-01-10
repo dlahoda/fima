3677517d83b721089b3960f469e6e75d4942bdb9
Denys Lahoda <denys.lahoda@gmail.com>
2019-01-10 18:50:31 +0200

    Made header responsive

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,5 @@
 //= "../../node_modules/popper.js/dist/umd/popper.js"
 //= "../../node_modules/bootstrap/js/dist/util.js"
 //= "../../node_modules/bootstrap/js/dist/carousel.js"
+//= "../../node_modules/bootstrap/js/dist/collapse.js"
+
